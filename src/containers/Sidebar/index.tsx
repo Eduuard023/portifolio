b0635c68a976c75d07042e156921ca8f043f3d67
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2023-11-16 15:44:35 -0300

    Criando a section sobre

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="http://github.com/Eduuard023.png" />
+    <Titulo fontSize={20}>Eduardo Felipe</Titulo>
+  </aside>
+)
+
+export default Sidebar

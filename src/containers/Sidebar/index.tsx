bfe38265c86d98900e9173892426ebf1d1e7e7ab
Eduardo Felipe <eduardofelipe_2000@hotmail.com>
2023-11-17 10:35:15 -0300

    criando a seção de projetos

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -3,7 +3,7 @@ import Titulo from '../../components/Titulo'
 const Sidebar = () => (
   <aside>
     <img src="http://github.com/Eduuard023.png" />
-    <Titulo fontSize={20}>Eduardo Felipe</Titulo>
+    <Titulo fontSize={20}>Eduardo</Titulo>
   </aside>
 )
 
